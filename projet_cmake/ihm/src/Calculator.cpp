a1e8a19534fbcb2a83ace48e712857c85775fb7a
Loick Piera <pyrech@gmail.com>
2013-03-30 09:41:53 +0100

    Update calcul of the pixel and add comment

--- projet_cmake/ihm/src/Calculator.cpp
+++ projet_cmake/ihm/src/Calculator.cpp
@@ -8,21 +8,35 @@ Calculator::Calculator(MainWindow *_parent)
 
 int Calculator::getPixel(float64* data) {
 	int start, end;
+	// Detect start and end pixel of the pic
 	detectPic(data, start, end);
-	std::cout << start << ";" << end << std::endl;
-	return round((start+end)/2);
+	return round((start+end)/2); // The central pixel is the one we want
 }
 
 void Calculator::detectPic(float64* data, int &start, int &end) {
 	start = 0;
 	end = 0;
-	float64 threshold = max(data)*(100-THRESHOLD_PIC_PERCENT)/100;
+	float64 max_signal = max(data);
+	// If max is not high enough, there must be not relevant pic to detect
+	if (max_signal < MIN_PIC_VALUE) {
+		return;
+	}
+	// Threshold which determinate where the pic starts and ends
+	float64 threshold = max_signal*(100-THRESHOLD_PIC_PERCENT)/100;
 	for(int i=0; i<MAX_PIXEL; i++) {
+		// Search for start if we haven't find it yet
 		if (start == 0 && data[i] > threshold) {
 			start = i+1;
 		}
+		// Search for end if start found and end not yet found
 		if (start != 0 && end == 0 && data[i+1] < threshold) {
 			end = i+1;
+		}
+		// If another pics, we can't know which is right
+		if (start != 0 && end != 0 && data[i] > threshold) {
+			start = 0;
+			end = 0;
+			return;
 		}
 	}
 }
